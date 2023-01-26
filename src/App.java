e31d7a9218db4382a1d644858e6de79676568a3c
SauceX <mahmoudzak4@gmail.com>
2023-01-26 15:32:36 +0300

    init

---
+++ src/App.java
@@ -0,0 +1,53 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        //         1- read file
+        //         2- Ask for method of generating groups (by group number, or by studnets per group)
+        //         3- Ask for parameters according to choice of method
+        //         4- Generate according to parameters
+        //         5- Print groups in a loop, and Generate a file with output
+    }
+        
+//     method to read students file, with try catch
+//         takes:
+//             -path to file (hardcoded), 
+//         returns:
+//             -a list of students
+
+//     method to create output file, with try catch
+//         takes:
+//             -path to file (same input path), 
+
+//     method to pick a student from given list, and remove him from the list:
+//         takes:
+//             -list
+//         returns:
+//             -picked student 
+
+//     method to distribute students accross groups (according to nubmer of groups), 
+//         takes:
+//             -list of students,
+//             -number of groups,
+//             -max number of studnets per group,
+//         returns:
+//             -list of group objects
+
+//     method to distribute students accross groups (according to nubmer of studetns per group), 
+//         takes:
+//             -list of students,
+//             -nubmer of studetns per group,
+//             -max number of studnets per group,
+//         returns:
+//             -list of group objects
+
+}
+
+
+// group class {
+//     list members (using max number per group)
+
+//     toString {
+//         with printf formatting for spacing (using String.format method as return)
+//         "group #: member1 , member2, ...
+//     }
+// }
