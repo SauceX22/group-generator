2c67b6573d365e1c149263878cf1e7f6f893d7b2
SauceX <mahmoudzak4@gmail.com>
2023-01-26 15:55:04 +0300

    created reading file method

--- src/App.java
+++ src/App.java
@@ -1,50 +1,65 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        //         1- read file
-        //         2- Ask for method of generating groups (by group number, or by studnets per group)
-        //         3- Ask for parameters according to choice of method
-        //         4- Generate according to parameters
-        //         5- Print groups in a loop, and Generate a file with output
+        // 1- read file
+        // 2- Ask for method of generating groups (by group number, or by studnets per
+        // group)
+        // 3- Ask for parameters according to choice of method
+        // 4- Generate according to parameters
+        // 5- Print groups in a loop, and Generate a file with output
     }
-        
-//     method to read students file, with try catch
-//         takes:
-//             -path to file (hardcoded), 
-//         returns:
-//             -a list of students
 
-//     method to create output file, with try catch
-//         takes:
-//             -path to file (same input path), 
+    public static List<String> readStudentsFile(String path) {
+        List<String> students = new ArrayList<String>();
+        try {
+            File file = new File(path);
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                students.add(line);
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        return students;
+    }
 
-//     method to pick a student from given list, and remove him from the list:
-//         takes:
-//             -list
-//         returns:
-//             -picked student 
+    // method to create output file, with try catch
+    // takes:
+    // -path to file (same input path),
 
-//     method to distribute students accross groups (according to nubmer of groups), 
-//         takes:
-//             -list of students,
-//             -number of groups,
-//             -max number of studnets per group,
-//         returns:
-//             -list of group objects
+    // method to pick a student from given list, and remove him from the list:
+    // takes:
+    // -list
+    // returns:
+    // -picked student
 
-//     method to distribute students accross groups (according to nubmer of studetns per group), 
-//         takes:
-//             -list of students,
-//             -nubmer of studetns per group,
-//             -max number of studnets per group,
-//         returns:
-//             -list of group objects
+    // method to distribute students accross groups (according to nubmer of groups),
+    // takes:
+    // -list of students,
+    // -number of groups,
+    // -max number of studnets per group,
+    // returns:
+    // -list of group objects
+
+    // method to distribute students accross groups (according to nubmer of studetns
+    // per group),
+    // takes:
+    // -list of students,
+    // -nubmer of studetns per group,
+    // -max number of studnets per group,
+    // returns:
+    // -list of group objects
 
 }
-
 
 class Group {
 
